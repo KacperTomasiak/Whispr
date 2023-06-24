14e6fae76ac2df7990d8483a7fe2adc7e209102b
Kacper Tomasiak <kacper.tomasiak@protonmail.com>
2023-06-24 15:09:16 +0200

    Add signup and login forms

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
   import Nav from "../../components/Nav.svelte";
+  import Form from "../../components/Form.svelte";
   import Footer from "../../components/Footer.svelte";
 </script>
 
 <Nav activeButton={1} />
+<div id="form-wrapper">
+  <Form type="login" />
+</div>
 <Footer />
+
+<style>
+  #form-wrapper {
+    width: 100%;
+    height: calc(100% - 180px);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
