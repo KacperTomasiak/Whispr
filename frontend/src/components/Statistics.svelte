2dc35a869537bfa515cda7520fc005ed7a7d6186
Kacper Tomasiak <kacper.tomasiak@protonmail.com>
2023-06-26 12:13:43 +0200

    Update the profile page

---
+++ frontend/src/components/Statistics.svelte
@@ -0,0 +1,60 @@
+<div id="content-wrapper">
+  <h2>Statistics</h2>
+  <div id="statistics">
+    <div id="number-of-sessions">
+      <h3>Number of sessions</h3>
+      <div class="block">0</div>
+    </div>
+    <div id="account-age">
+      <h3>Account age (days)</h3>
+      <div class="block">0</div>
+    </div>
+  </div>
+</div>
+
+<style>
+  #content-wrapper {
+    width: 100%;
+    height: 220px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+    margin-top: 80px;
+  }
+
+  h2 {
+    font-size: 2.8rem;
+  }
+
+  #statistics {
+    width: 80%;
+    height: 150px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  h3 {
+    font-size: 2rem;
+  }
+
+  #number-of-sessions,
+  #account-age {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+    height: 100%;
+  }
+
+  .block {
+    background-color: #06d6a0;
+    color: #131516;
+    width: auto;
+    height: auto;
+    padding: 20px;
+    border-radius: 10px;
+    font-size: 4.8rem;
+  }
+</style>
---
+++ frontend/src/components/UserInformation.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+  import Button from "./Button.svelte";
+</script>
+
+<div id="content-wrapper">
+  <div id="user">
+    <div id="profile-picture" />
+    <div id="nickname">$test_user</div>
+    <Button message="Save" isActive={true} link="none" />
+  </div>
+  <div id="information">
+    <div id="picture">
+      <h3>Profile Picture</h3>
+      <div class="block" />
+    </div>
+    <div id="username">
+      <h3>Username</h3>
+      <input
+        type="text"
+        name="username"
+        id="username-input"
+        value="$test_user"
+      />
+    </div>
+  </div>
+</div>
+
+<style>
+  #content-wrapper {
+    width: 100%;
+    height: 220px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+  }
+
+  #user {
+    width: 80%;
+    height: 60px;
+    border-radius: 20px;
+    background-color: #181d20;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    padding: 0px 20px;
+  }
+
+  #profile-picture {
+    width: 40px;
+    height: 40px;
+    border-radius: 10px;
+    background-color: #06d6a0;
+    margin-right: 10px;
+  }
+
+  #nickname {
+    font-size: 2.8rem;
+    flex: 1;
+  }
+
+  #information {
+    width: 80%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  #picture,
+  #username {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+    height: 100px;
+  }
+
+  h3 {
+    font-size: 2rem;
+  }
+
+  .block {
+    background-color: #06d6a0;
+    width: 65px;
+    height: 65px;
+    border-radius: 10px;
+  }
+
+  input {
+    border: none;
+    border-radius: 20px;
+    background-color: #181d20;
+    color: #06d6a0;
+    width: 150px;
+    height: 40px;
+    font-size: 2rem;
+    outline: none;
+    text-align: center;
+  }
+</style>
