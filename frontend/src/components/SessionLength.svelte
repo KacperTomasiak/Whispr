186801a5ec32ad6ad442d8709f5fd738bc53c7b4
Kacper Tomasiak <kacper.tomasiak@protonmail.com>
2023-06-26 13:55:24 +0200

    Update the settings page

---
+++ frontend/src/components/SessionLength.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import Button from "./Button.svelte";
+</script>
+
+<div id="content-wrapper">
+  <h2>Session Length</h2>
+  <h4>This refers to the time after which you will be logged out.</h4>
+  <div id="session-length">
+    <Button message="15 minutes" isActive={true} link="none" />
+    <Button message="30 minutes" isActive={true} link="none" />
+    <Button message="1 hour" isActive={true} link="none" />
+  </div>
+</div>
+
+<style>
+  #content-wrapper {
+    width: 100%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+    text-align: center;
+    margin-top: 80px;
+  }
+
+  h2 {
+    font-size: 2.8rem;
+    margin: 10px 40px;
+  }
+
+  h4 {
+    font-size: 1.4rem;
+    margin: 10px 40px;
+  }
+
+  #session-length {
+    width: 80%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    margin: 10px;
+  }
+</style>
---
+++ frontend/src/components/Themes.svelte
@@ -0,0 +1,95 @@
+<div id="content-wrapper">
+  <h2>Themes</h2>
+  <div id="themes">
+    <div class="theme active">
+      <div class="first-color first-theme" />
+      <div class="second-color first-theme" />
+    </div>
+    <div class="theme">
+      <div class="first-color second-theme" />
+      <div class="second-color second-theme" />
+    </div>
+    <div class="theme">
+      <div class="first-color third-theme" />
+      <div class="second-color third-theme" />
+    </div>
+  </div>
+</div>
+
+<style>
+  #content-wrapper {
+    width: 100%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: column;
+  }
+
+  h2 {
+    font-size: 2.8rem;
+  }
+
+  #themes {
+    width: 80%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
+
+  .theme {
+    width: 220px;
+    height: 150px;
+    border-radius: 20px;
+    border: 3px dashed white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 20px;
+    cursor: pointer;
+  }
+
+  .first-color {
+    width: 50%;
+    height: 100%;
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+  }
+
+  .second-color {
+    width: 50%;
+    height: 100%;
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+  }
+
+  .first-color.first-theme {
+    background-color: #06d6a0;
+  }
+
+  .second-color.first-theme {
+    background-color: #131516;
+  }
+
+  .first-color.second-theme {
+    background-color: #38a3a5;
+  }
+
+  .second-color.second-theme {
+    background-color: #22577a;
+  }
+
+  .first-color.third-theme {
+    background-color: #fdd545;
+  }
+
+  .second-color.third-theme {
+    background-color: #202628;
+  }
+
+  .theme.active {
+    border: 3px solid white;
+  }
+</style>
