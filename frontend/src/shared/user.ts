11a6f754e1b30cab1cfd160e38452e4fa83e89da
Kacper Tomasiak <kacper.tomasiak@protonmail.com>
2023-09-06 22:09:09 +0200

    Display number of sessions

--- frontend/src/shared/user.ts
+++ frontend/src/shared/user.ts
@@ -3,6 +3,7 @@ import { writable, type Writable } from "svelte/store";
 export let privateKey: Writable<string> = writable("");
 export let username: Writable<string> = writable("");
 export let accountAge: Writable<number> = writable(0);
+export let numberOfSessions: Writable<number> = writable(0);
 export let sessions: Writable<string[]> = writable([]);
 
 export const getUserData = async (): Promise<any> => {
@@ -22,5 +23,6 @@ export const syncUserData = async (): Promise<void> => {
   privateKey.set(result.privateKey);
   username.set(result.username);
   accountAge.set(result.accountAge);
+  numberOfSessions.set(result.numberOfSessions);
   sessions.set(result.sessions);
 };
