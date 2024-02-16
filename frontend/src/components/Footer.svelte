e1002989872660d9db5aff6934f379f0942aa612
Kacper Tomasiak <118994310+KacperTomasiak@users.noreply.github.com>
2024-02-16 21:55:54 +0100

    Add a footer

---
+++ frontend/src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<footer>Whispr. | A privacy focused chat application</footer>
+
+<style>
+  footer {
+    width: 100%;
+    height: 50px;
+    font-size: 1.6rem;
+    text-align: center;
+    position: absolute;
+    bottom: 0px;
+  }
+</style>
