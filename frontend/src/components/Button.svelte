a97e9640f82fe4e9ff8ad7f9706b35632a088010
Kacper Tomasiak <118994310+KacperTomasiak@users.noreply.github.com>
2024-02-16 21:55:54 +0100

    Add a navigation bar

---
+++ frontend/src/components/Button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let message: string;
+  export let isActive: boolean;
+  export let link: string;
+</script>
+
+<a href="/{link}"><button class:active={isActive}>{message}</button></a>
+
+<style>
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    width: 120px;
+    height: 40px;
+    border-radius: 20px;
+    border: none;
+    font-size: 2.4rem;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  .active {
+    color: #131516;
+    background-color: #06d6a0;
+  }
+</style>
